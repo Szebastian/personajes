7b9d399b98bbc4c225277c95426c11325b270911
Szebastian <sebastianmarcoscontreras@gmail.com>
2023-12-13 16:22:32 -0300

    lista de personajes

---
+++ src/app/dbz/pages/main-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-pages.component.html'
+})
+
+export class MainPageComponent {
+
+  constructor(public dbzService: DbzService) {
+
+  }
+
+}
